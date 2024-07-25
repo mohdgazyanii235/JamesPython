c0f027d46bf5eac0ba584b0657474585e2e42cb5
Mohamed Yusuf Mohamed Javid <mohdgazyanii235@gmail.com>
2024-07-25 18:22:21 +0100

    new homework

---
+++ homework/homework2.py
@@ -0,0 +1,2 @@
+# Hello James, next homework
+# create a function called square_root(num) which takes a parameter num and returns the square root of the number. if no square root is found, return False
