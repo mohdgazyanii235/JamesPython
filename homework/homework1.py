f00506e1558f057819e80986c9d5171adcd3b93e
Mohamed Yusuf Mohamed Javid <mohdgazyanii235@gmail.com>
2024-07-25 17:53:30 +0100

    changes added to homework1

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,5 +1,9 @@
 # Hello James, your first homework for today is to create a python function can given a parameter
 # return True, if the parameter is even and False if the parameter is odd.
+
+
+# Hi James, I like this work but I want you to make the following changes:
+# - I want the function to keep asking the user for numbers until the user types 'q'
 def is_even(x):
     if x % 2 == 0:
         return True
