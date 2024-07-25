6d116e9990e390def286109f4ba257e2b9b0d6b0
James Lock <jameslock@Jamess-MacBook-Pro.local>
2024-07-25 17:51:03 +0100

    Homework 1 completed

--- homework/homework1.py
+++ homework/homework1.py
@@ -1,2 +1,9 @@
 # Hello James, your first homework for today is to create a python function can given a parameter
 # return True, if the parameter is even and False if the parameter is odd.
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+x = int(input("Enter a number of choice:"))    
+print(is_even(x))
