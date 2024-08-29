7f99e41e23066d6e4e6c09fe051de79f8e8e301d
Mohamed Yusuf <43543845+mohdgazyanii235@users.noreply.github.com>
2024-08-29 15:26:44 +0400

    questoin

---
+++ class9.py
@@ -0,0 +1,7 @@
+question = [1, 2, 5, 9, 13, 18, 27, 28, 31]
+
+
+def numberExists(question, num):
+    # check if num exists in question?
+    # return true if it exists, return false if it doesn't exist.
+    pass
